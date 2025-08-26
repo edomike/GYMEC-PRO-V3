065da4b09278f0c8e58de5fd1421451a99309762
edomike <miguel.rrnn@gmail.com>
2025-08-26 13:39:18 -0400

    Update sw.js
    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE = "gymec-pro-v2";
+const CACHE = "gymec-pro-v3"; // <- subimos versión
+
 const ASSETS = [
   "./",
   "index.html",
@@ -8,20 +9,22 @@ const ASSETS = [
   "assets/icon-512.png"
 ];
 
+// Activación inmediata y control de clientes
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
 });
-
 self.addEventListener("activate", (e) => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
+    Promise.all([
+      caches.keys().then(keys =>
+        Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+      ),
+      self.clients.claim()
+    ])
   );
+});
+self.addEventListener("fetch", (e) => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
-});
